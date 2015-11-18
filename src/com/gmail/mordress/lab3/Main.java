614504f57f4450937bba00b98f9038f701752179
Mordress <Mordress@gmail.com>
2015-11-18 20:50:17 +0300

    refactoring

--- src/com/gmail/mordress/lab3/Main.java
+++ src/com/gmail/mordress/lab3/Main.java
@@ -24,7 +24,7 @@ import java.util.List;
 * */
 public class Main {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         VoucherXmlValidator validator = new VoucherXmlValidator("resources" + File.separator + "Vouchers.xml");
 
         System.out.print("Validate vouchers.xml: ");
